146ff3039c24fb3d99fa506691402869a77cf0fb
Ed Brannin <edbrannin@gmail.com>
2017-03-01 08:44:28 -0500

    Wireframe logic

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
 const app = document.createElement('main');
 const text = document.createTextNode('Hello world!');
 
+document.title = "54-Day Rosary Novena"
+
 app.appendChild(text);
 document.getElementById('root').appendChild(app);
+
+const mysteries = [
+  "Joyful",
+  "Sorrowful",
+  "Glorious"
+];
+
+function currentDay() {
+  //Temporarily hardcode start to Ash Wednesday, 2017
+  const startDay = null;
+  const today = null;
+  const dayIndex = 0;
+  return dayIndex;
+}
+
+
