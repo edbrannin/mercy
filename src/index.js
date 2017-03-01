73faf1e3daf4dd888dfab9ff8c94c39e5259f672
Ed Brannin <edbrannin@gmail.com>
2017-03-01 17:14:53 -0500

    Add petition prayer; commented-out thanksgiving prayer.

--- src/index.js
+++ src/index.js
@@ -35,10 +35,14 @@ function mysteriesFor(dayIndex) {
   return mysteries[dayIndex % mysteries.length];
 }
 
+// Petition Prayer (27 Days): Blessed Mother, hear my plea and bring it before the throne of your Son, my Lord, Jesus Christ. Please look with favor on this devotion, and grant me [say your intention here.] I ask these things of you, my Mother, in the name of the Father, and of the Son, and of the Holy Spirit. Amen.
+// Thanksgiving Prayer (27 Days): Blessed Mother, thank you for hearing my prayer and and interceding on my behalf. Mary, Mother of my Soul, be with me all my days, and accept my humble thanks for your many gifts, which I accept in the name of the Father, and of the Son, and of the Holy Spirit. Amen.
+
 [
   "Day " + (currentDay() + 1),
   "Mysteries: " + mysteriesFor(currentDay()),
   "Addition Type: " + additionType(currentDay()),
+  "Blessed Mother, hear my plea and bring it before the throne of your Son, my Lord, Jesus Christ. Please look with favor on this devotion, and grant me [say your intention here.] I ask these things of you, my Mother, in the name of the Father, and of the Son, and of the Holy Spirit. Amen."
 ].forEach(function(i) {
   console.log(i);
   const p = document.createElement("p")
