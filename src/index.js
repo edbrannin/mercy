713a622aef0cba7bd278252cdce0fe13b9e0dab5
Ed Brannin <edbrannin@gmail.com>
2017-04-15 02:46:52 -0400

    Converted 54-day novena to Divine Mercy Novena

--- src/index.js
+++ src/index.js
@@ -1,67 +1,33 @@
-const mysteries = [
-  "joyful",
-  "sorrowful",
-  "glorious"
-];
-
 function currentDay() {
   //Temporarily hardcode start to Ash Wednesday, 2017
-  const ashWeds = moment("2017-03-01");
+  const goodFriday = moment("2017-04-14");
   const today = moment();
-  const dayIndex = today.diff(ashWeds, 'days') // 0
+  const dayIndex = today.diff(goodFriday, 'days') // 0
   return dayIndex;
 }
 
-function intentionType(dayIndex) {
-  if (dayIndex < 26) {
-    return "petition";
-  } else if (dayIndex < 54) {
-    return "thanksgiving";
-  } else {
-    return "done";
-  }
-}
-
-function mysteriesFor(dayIndex) {
-  return mysteries[dayIndex % mysteries.length];
+function dayData(dayIndex) {
+  return prayers.novena[dayIndex % prayers.novena.length];
 }
 
 
 jQuery(function() {
   const todayIndex = currentDay();
+  const todayData = dayData(todayIndex);
 
   jQuery('#day-number').text(
     "Day " + (todayIndex + 1)
   );
 
-  const mysteries = mysteriesFor(todayIndex);
-  jQuery('#mysteries').text(
-    "Mysteries: " + mysteriesFor(todayIndex)
-  );
-  jQuery('#toc .' + mysteries).tab('show');
-
-  const todayIntentionType = intentionType(todayIndex);
+  console.log("Prayers:", prayers.novena);
+  console.log("index:", todayIndex);
+  console.log("Data:", todayData);
   jQuery('#intention-type').text(
-    "Intention Type: " + todayIntentionType
+    todayData.tagline
   );
+
+  jQuery('#toc .day' + todayIndex).tab('show');
 
   // document.getElementById('closing-prayer').appendChild()
-
-  jQuery('#closing-prayer div.prayer').
-    hide().
-      //each(function(){console.log("Hide", this)}).
-        filter('.' + todayIntentionType).
-          //each(function(){console.log("Show",this)}).
-            show();
-  //jQuery('#closing-prayer ol li').hide().filter('.' + todayIntentionType).show();
-  jQuery('#closing-prayer ol li').
-    hide().
-      //each(function(){console.log("Hide", this)}).
-        filter('.' + todayIntentionType).
-          //each(function(){console.log("Show",this)}).
-            show();
-  jQuery('#closing-toc a.singular.' + todayIntentionType).
-    each(function(){console.log("Tab", this)}).
-      tab('show');
 
 });
